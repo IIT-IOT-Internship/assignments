46965676cea58694fa2f75a6d2ef219582818f17
siddhipatil0181-eng <siddhipatil0181@gmail.com>
2025-12-12 20:59:42 +0530

    day1

---
+++ day1/demo2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculating sum
+sum_result = num1 + num2
+
+# Displaying the result
+print("The sum of the two numbers is:", sum_result)
