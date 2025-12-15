a2874fd4314c7c8b302ec8df5d72d5bc1afb288f
siddhipatil0181-eng <siddhipatil0181@gmail.com>
2025-12-15 23:06:51 +0530

    day3classwk

--- day1/demo3.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-# Calculating sum
-sum_result = num1 + num2
-
-# Displaying the result
-print("The sum of the two numbers is:", sum_result)
